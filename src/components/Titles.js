b255de01cef1781e38724cfadb077f9e954a860d
amngpta <amangranchi@gmail.com>
2019-04-28 15:33:33 +0530

    Add code

---
+++ src/components/Titles.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+  <h1>Welcome to Weather App</h1>
+  <p>Find out temperature, condition and more ...</p>
+</div>
+);
+
+/*class Titles extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Welcome to Weather App</h1>
+      </div>
+    );
+  }
+}*/
+
+export default Titles;
